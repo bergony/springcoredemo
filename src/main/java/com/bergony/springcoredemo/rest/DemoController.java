77a4f1cf2e472de56cbfbc7db556719d8c7dc12b
bergony.bandeira <bergony.bandeira@quark.tec.br>
2023-06-12 21:14:55 -0300

    @Qualifer

--- src/main/java/com/bergony/springcoredemo/rest/DemoController.java
+++ src/main/java/com/bergony/springcoredemo/rest/DemoController.java
@@ -2,16 +2,17 @@ package com.bergony.springcoredemo.rest;
 
 import com.bergony.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    private Coach myCoach;
+    private final Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach myCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach myCoach) {
         this.myCoach = myCoach;
     }
 
