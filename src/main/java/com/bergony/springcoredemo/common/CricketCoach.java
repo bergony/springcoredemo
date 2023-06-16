90b86a6d8520151ca392d4c5a3e8850ef4c31281
bergony.bandeira <bergony.bandeira@quark.tec.br>
2023-06-16 20:25:23 -0300

    @Configuration @Bean method

--- src/main/java/com/bergony/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/bergony/springcoredemo/common/CricketCoach.java
@@ -1,7 +1,5 @@
 package com.bergony.springcoredemo.common;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,19 +7,6 @@ public class CricketCoach implements Coach{
     public CricketCoach() {
         System.out.println("In constructor: "+ getClass().getSimpleName());
     }
-
-    @PostConstruct
-    public void doMyStartupStuff() {
-        System.out.println("In doMyStartupStuff(): "  + getClass().getSimpleName());
-    }
-
-
-    @PreDestroy
-    public void doMyCleanupStuff() {
-        System.out.println("In doMyCleanupStuff(): "  + getClass().getSimpleName());
-    }
-
-
 
     @Override
     public String getDailyWorkout() {
